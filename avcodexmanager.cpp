1eb25335bbd3e2da35d6c7b7d18297910af447cb
Tiny Oh <wuxiao@saferycom.com>
2020-06-10 09:57:12 +0800

    add AvCodexManaget member func.

--- avcodexmanager.cpp
+++ avcodexmanager.cpp
@@ -1,5 +1,39 @@
 #include "avcodexmanager.h"
 
+AvCodexManager *AvCodexManager::m_instance = new AvCodexManager;
+
+class SwsScaleContext
+{
+public:
+    SwsScaleContext()
+    {
+
+    }
+    void SetSrcResolution(int width, int height)
+    {
+        srcWidth = width;
+        srcHeight = height;
+    }
+
+    void SetDstResolution(int width, int height)
+    {
+        dstWidth = width;
+        dstHeight = height;
+    }
+    void SetFormat(AVPixelFormat iformat, AVPixelFormat oformat)
+    {
+        this->iformat = iformat;
+        this->oformat = oformat;
+    }
+public:
+    int srcWidth;
+    int srcHeight;
+    int dstWidth;
+    int dstHeight;
+    AVPixelFormat iformat;
+    AVPixelFormat oformat;
+};
+
 AvCodexManager::AvCodexManager()
 {
 
--- avcodexmanager.h
+++ avcodexmanager.h
@@ -1,11 +1,43 @@
 #ifndef AVCODEXMANAGER_H
 #define AVCODEXMANAGER_H
 
+extern "C"
+{
+    #include "libavutil/opt.h"
+    #include "libavutil/channel_layout.h"
+    #include "libavutil/common.h"
+    #include "libavutil/imgutils.h"
+    #include "libavutil/mathematics.h"
+    #include "libavutil/samplefmt.h"
+    #include "libavutil/time.h"
+    #include "libavutil/fifo.h"
+    #include "libavcodec/avcodec.h"
+    #include "libavformat/avformat.h"
+    #include "libavformat/avio.h"
+    #include "libavfilter/avfilter.h"
+    #include "libavfilter/buffersink.h"
+    #include "libavfilter/buffersrc.h"
+    #include "libswscale/swscale.h"
+    #include "libswresample/swresample.h"
+    #include "libavdevice/avdevice.h"
+}
 
 class AvCodexManager
 {
-public:
+    static AvCodexManager *m_instance;
+    static AvCodexManager *getInstance()
+    {
+        return m_instance;
+    }
+private:
     AvCodexManager();
+    AVFormatContext *inputContext = nullptr;
+    AVCodecContext *encodeContext = nullptr;
+    AVFormatContext * outputContext;
+    int64_t lastReadPacktTime ;
+    int64_t packetCount = 0;
+    struct SwsContext* pSwsContext = nullptr;
+    uint8_t * pSwpBuffer = nullptr;
 };
 
 #endif // AVCODEXMANAGER_H
